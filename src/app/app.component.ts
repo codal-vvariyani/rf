5e501026e01cc91c39f8f2268e11a36aa01bc4ec
codal-vvariyani <vvariyani@codal.com>
2019-01-17 19:29:36 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rf';
+
+  registrationForm = new FormGroup({
+    userName: new FormControl('abc'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
+  });
 }
