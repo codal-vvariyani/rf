d77c5f563fca68fe05812a883d7c9133deeabbb9
codal-vvariyani <vvariyani@codal.com>
2019-01-18 17:05:03 +0530

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { PasswordValidator } from 'src/shared/password-validator';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,33 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class AppComponent {
   title = 'rf';
+  constructor(
+    private fb: FormBuilder
+  ){}
 
-  registrationForm = new FormGroup({
-    userName: new FormControl('abc'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      postalCode: new FormControl('')
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  registrationForm = this.fb.group({
+    userName: ['abc',[Validators.required, Validators.minLength(3)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state:[''],
+      postalCode:['']
     })
-  });
+  },{validator: PasswordValidator});
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('abc'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
 }
